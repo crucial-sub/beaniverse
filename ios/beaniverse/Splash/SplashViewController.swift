7cd6f9d5bc887b5d89b7f13fdb52a9c64cbf8d5f
crucial-sub <jssub940@gmail.com>
2024-08-04 00:32:03 +0900

    스토리보드 제거 및 스플래시 스크린 수정

--- ios/beaniverse/Splash/SplashViewController.swift
+++ ios/beaniverse/Splash/SplashViewController.swift
@@ -1,14 +1,25 @@
 import UIKit
 
 @objc class SplashViewController: UIViewController {
-    
+  
     private let splashImageView: UIImageView = {
         let imageView = UIImageView(frame: .zero)
+        
+        
         imageView.image = UIImage(named: "SplashScreen")
         imageView.contentMode = .scaleAspectFit
         imageView.translatesAutoresizingMaskIntoConstraints = false
         return imageView
     }()
+  
+  private let titleLabel: UILabel = {
+          let label = UILabel()
+          label.text = "Beaniverse"
+          label.textColor = .white
+          label.font = UIFont.systemFont(ofSize: 28, weight: .bold)
+          label.translatesAutoresizingMaskIntoConstraints = false
+          return label
+      }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,12 +29,16 @@ import UIKit
     private func setupUI() {
         view.backgroundColor = UIColor(red: 217/255, green: 115/255, blue: 66/255, alpha: 1.0)  // 오렌지 배경색
         view.addSubview(splashImageView)
+        view.addSubview(titleLabel)
         
         NSLayoutConstraint.activate([
-            splashImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            splashImageView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
-            splashImageView.widthAnchor.constraint(equalTo: view.widthAnchor),
-            splashImageView.heightAnchor.constraint(equalTo: view.heightAnchor)
+          splashImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+          splashImageView.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: -50),
+          splashImageView.widthAnchor.constraint(equalToConstant: 79),
+          splashImageView.heightAnchor.constraint(equalToConstant: 66.08),
+            
+          titleLabel.topAnchor.constraint(equalTo: splashImageView.bottomAnchor, constant: 20),
+          titleLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor)
         ])
     }
 }
